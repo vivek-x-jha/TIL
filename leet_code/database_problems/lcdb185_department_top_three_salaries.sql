8b0311196c7f72500c185d25d8a7db4433eb7b1c
Vivek Jha <vivek.x.jha@gmail.com>
2020-03-02 17:27:14 -0600

    Added leetcode dept top 3 salaries sql problem

---
+++ leet_code/database_problems/lcdb185_department_top_three_salaries.sql
@@ -0,0 +1,30 @@
+/*
+ Leetcode Database Practice
+ Difficulty: Hard
+ Url: https://leetcode.com/problems/department-top-three-salaries/
+
+ The Employee table holds all employees. Every employee has an Id, and there is also a column for the department Id.
+ The Department table holds all departments of the company.
+ Write a SQL query to find employees who earn the top three salaries in each of the department.
+ For the above tables, your SQL query should return the following rows (order of rows does not matter).
+ */
+
+# Write your MySQL query statement below
+with merged as (
+    select
+        d.name as Department,
+        e.name as Employee,
+        e.salary as Salary
+    from employee e
+    left join department d
+    on e.departmentid=d.id
+),
+ranked as (
+    select
+        Department,
+        Employee,
+        Salary,
+        dense_rank() OVER (partition by department order by salary desc) as rnk
+    from merged
+)
+select * from ranked where rnk <= 3;
