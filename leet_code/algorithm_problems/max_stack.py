e1af3dacf22a03063a7a37b51efb64013f842cce
Vivek Jha <vivek.x.jha@gmail.com>
2020-03-17 22:53:55 -0500

    setup class outline for max stack problem - need to think how to implement

---
+++ leet_code/algorithm_problems/max_stack.py
@@ -0,0 +1,20 @@
+# Implement a stack that has the following methods:
+#
+# push(val): push val on to the stack
+# pop(): pop off and return top most element of stack. If there are no elements in the stack, throw an error.
+# max(): return the maximum value in the stack currently. If there are no elements in the stack, throw an error.
+#
+# Each method should run in constant time
+
+class Max_stack:
+	def __init__(self):
+		pass
+
+	def push(self, val):
+		pass
+
+	def pop(self):
+		pass
+
+	def max(self):
+		pass
